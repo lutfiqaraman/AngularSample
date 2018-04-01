198f0d7c8f9a595bbd37a8f4bd12c337997764d6
lutfiqaraman <lutfiqaraman@gmail.com>
2018-04-01 22:03:32 +0300

    Adding filter function

--- JS/script.js
+++ JS/script.js
@@ -86,3 +86,17 @@ angular
                 technology.dislikes++;
         };
     });
+
+angular
+    .module("FilterModule", [])
+    .controller("FilterController", function ($scope) {
+        $scope.employees = [
+            {name: "Ben", dateOfBirth: new Date("November 23, 1980"), gender: "Male", salary: 55000.788},
+            {name: "Sara", dateOfBirth: new Date("May 05, 1970"), gender: "Female", salary: 75000},
+            {name: "Mark", dateOfBirth: new Date("August 15, 1974"), gender: "Male", salary: 50000},
+            {name: "Pam", dateOfBirth: new Date("October 27, 1979"), gender: "Female", salary: 50000},
+            {name: "Todd", dateOfBirth: new Date("December 30, 1983"), gender: "Male", salary: 60000}
+        ];
+
+        $scope.rowLimit = 3;
+    });
